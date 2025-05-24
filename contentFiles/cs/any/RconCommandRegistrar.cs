e32331723572bc179af1cce11b344692a1cff9d4
Mark Vaaz <marcosrodolfo32@gmail.com>
2025-05-24 20:37:29 -0300

    Added Souce Support for RconCommandRegistrar.cs

--- contentFiles/cs/any/RconCommandRegistrar.cs
+++ contentFiles/cs/any/RconCommandRegistrar.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace YourNamespace;
+namespace ScarletRCON.Shared;
 
 [AttributeUsage(AttributeTargets.Class)]
 public class RconCommandCategoryAttribute(string groupName) : Attribute {
