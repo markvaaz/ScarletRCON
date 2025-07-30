7f6b0eb8a7d64f949abe2054cf2b7db04d03b42a
Mark Vaaz <marcosrodolfo32@gmail.com>
2025-07-30 11:51:44 -0300

    -

--- CommandSystem/CommandHandler.cs
+++ CommandSystem/CommandHandler.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
+using ProjectM;
 
 namespace ScarletRCON.CommandSystem;
 
